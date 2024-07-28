4bf6aa8c4e44123fa8e48450506869efa716a51a
Krish Mittal <krishmittal212005@gmail.com>
2024-07-28 17:07:40 +0530

    google auth

--- dashboard/AuthenticationViewModel.swift
+++ dashboard/AuthenticationViewModel.swift
@@ -174,6 +174,47 @@ extension AuthenticationViewModel {
 }
 
 extension AuthenticationViewModel {
+    func signInWithGoogle() async -> Bool {
+        
+        guard let clientID = FirebaseApp.app()?.options.clientID else {
+            fatalError("No client ID found in Firebase Config")
+        }
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first,
+              let rootViewController = window.rootViewController else {
+            print("There is no root view controller!")
+            return false
+            
+        }
+        do {
+            let userAuthentication = try await GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController)
+            
+            let user = userAuthentication.user
+            guard let idToken = user.idToken else { throw AuthenticationError.tokenError(message: "ID token missing") }
+            let accessToken = user.accessToken
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken.tokenString,
+                                                           accessToken: accessToken.tokenString)
+            
+            let result = try await Auth.auth().signIn(with: credential)
+            let firebaseUser = result.user
+            firstName = firebaseUser.displayName ?? ""
+            email = firebaseUser.email ?? ""
+            insertUserRecord(id: currentUserId)
+            return true
+        }
+        catch {
+            print(error.localizedDescription)
+            self.errorMessage = error.localizedDescription
+            return false
+        }
+    }
+}
+
+extension AuthenticationViewModel {
     private func insertUserRecord(id: String) {
         let now = Date().timeIntervalSince1970
         
