a90ce3e7dc25fdd0d623c0cf57d58244f44ef616
Krish Mittal <krishmittal212005@gmail.com>
2024-07-28 14:59:39 +0530

    Initial Commit

---
+++ dashboard/dashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  dashboardApp.swift
+//  dashboard
+//
+//  Created by Krish Mittal on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct dashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
