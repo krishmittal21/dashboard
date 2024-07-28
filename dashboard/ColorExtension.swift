6c70ac5785302d67cfb00e2156b103479e83c803
Krish Mittal <krishmittal212005@gmail.com>
2024-07-28 15:22:42 +0530

    Colors

---
+++ dashboard/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  dashboard
+//
+//  Created by Krish Mittal on 28/07/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryColor = Color("BrandPurpleColor")
+    static let accentColor = Color("BrandPurpleAccentColor")
+}
