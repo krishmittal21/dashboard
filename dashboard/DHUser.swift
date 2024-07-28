e81b340bdadec6833c241fc5cb465225c833035e
Krish Mittal <krishmittal212005@gmail.com>
2024-07-28 16:27:49 +0530

    DHUser

--- dashboard/DHUser.swift
+++ dashboard/DHUser.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct DHUser: Encodable, Hashable, Identifiable {
+    let id: String
+    let tenantId: String
+    var role: String
+    var email: String
+    var firstName: String
+    var lastName: String
+    var phone: String
+    var avatarURL: String?
+    let joined: TimeInterval
+    var lastUpdated: TimeInterval
+    let createdBy: String
+    var editedBy: String
+    var status: String
+    var sysState: String
+    
+    var fullName: String {
+        return "\(firstName) \(lastName)"
+    }
+}
