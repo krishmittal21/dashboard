d768dcd9c3d6a12ae12494dd70b7e673ebf24ab2
Krish Mittal <krishmittal212005@gmail.com>
2024-07-29 15:12:05 +0530

    extract galleryContent to a viewbuilder

--- dashboard/View/GalleryView.swift
+++ dashboard/View/GalleryView.swift
@@ -17,62 +17,72 @@ struct GalleryView: View {
     
     var body: some View {
         GeometryReader { geometry in
-            ScrollView {
-                VStack(alignment: .leading, spacing: 20) {
-                    HStack(spacing: 10) {
-                        Text("Gallery")
-                            .customFont(.light, 25)
-                        VStack {
-                            Image(systemName: "photo")
-                            Spacer()
-                            Text("")
-                        }
-                    }
-                    .padding(.horizontal)
-                    
-                    HStack(spacing: 15) {
-                        Image(systemName: "square.grid.2x2")
-                        Image(systemName: "calendar")
-                    }
-                    .padding(.horizontal)
-                    
-                    ForEach(Array(viewModel.imageGroups.keys.sorted(by: >)), id: \.self) { sessionId in
-                        if let images = viewModel.imageGroups[sessionId] {
-                            VStack(alignment: .leading, spacing: 5) {
-                                HStack {
-                                    Text(relativeDate(for: images[0].dateCreated))
-                                        .font(.subheadline)
-                                        .foregroundColor(.secondary)
-                                    Spacer()
-                                    Text(formattedDate(for: images[0].dateCreated))
-                                        .font(.subheadline)
-                                        .foregroundColor(.secondary)
-                                }
-                                .frame(width: geometry.size.width / 2 - 5)
-                                
-                                LazyVGrid(columns: columns, spacing: 10) {
-                                    ForEach(images) { image in
-                                        Image(uiImage: image.image)
-                                            .resizable()
-                                            .scaledToFill()
-                                            .frame(width: (geometry.size.width / 2 - 5) / 2, height: (geometry.size.width / 2 - 5) / 2)
-                                            .clipped()
-                                            .cornerRadius(5)
-                                            .id(image.id)
-                                    }
-                                }
-                                .frame(width: geometry.size.width / 2 - 5)
-                            }
-                            .padding(.horizontal, 20)
-                        }
-                    }
-                }
-                .padding(.vertical)
+            HStack(spacing: 0) {
+                Spacer()
+                galleryContent(in: geometry)
             }
+            galleryContent(in: geometry)
         }
         .onAppear {
             viewModel.fetchData()
         }
+    }
+    
+    @ViewBuilder
+    func galleryContent(in geometry: GeometryProxy) -> some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                HStack(spacing: 10) {
+                    Text("Gallery")
+                        .customFont(.light, 25)
+                    VStack {
+                        Image(systemName: "photo")
+                        Spacer()
+                        Text("")
+                    }
+                }
+                .padding(.horizontal)
+                
+                HStack(spacing: 15) {
+                    Image(systemName: "square.grid.2x2")
+                    Image(systemName: "calendar")
+                }
+                .padding(.horizontal)
+                
+                ForEach(Array(viewModel.imageGroups.keys.sorted(by: >)), id: \.self) { sessionId in
+                    if let images = viewModel.imageGroups[sessionId] {
+                        VStack(alignment: .leading, spacing: 5) {
+                            HStack {
+                                Text(relativeDate(for: images[0].dateCreated))
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                Spacer()
+                                Text(formattedDate(for: images[0].dateCreated))
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+                            .frame(width: geometry.size.width / 2 - 20)
+                            
+                            LazyVGrid(columns: columns, spacing: 10) {
+                                ForEach(images) { image in
+                                    Image(uiImage: image.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: (geometry.size.width / 2 - 20) / 2, height: (geometry.size.width / 2 - 20) / 2)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                        .id(image.id)
+                                }
+                            }
+                            .frame(width: geometry.size.width / 2 - 20)
+                        }
+                        .padding(.horizontal, 20)
+                    }
+                }
+            }
+            .padding(.vertical)
+        }
+        .frame(width: geometry.size.width / 2)
     }
     
     func relativeDate(for date: Date) -> String {
