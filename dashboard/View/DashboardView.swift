fa8d37edbde70e262e3e6ee18ceb6fb0901b3fd6
Krish Mittal <krishmittal212005@gmail.com>
2024-07-29 09:03:04 +0530

    dashboard ui

--- dashboard/View/DashboardView.swift
+++ dashboard/View/DashboardView.swift
@@ -9,16 +9,49 @@ import SwiftUI
 
 struct DashboardView: View {
     @StateObject private var authenticationViewModel = AuthenticationViewModel()
+    @State private var showProfile: Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        HStack {
-            Image(systemName: "rectangle.portrait.and.arrow.right")
-            Text("Sign out")
-        }
-        .foregroundStyle(.red)
-        .onTapGesture {
-            authenticationViewModel.signOut()
+        NavigationStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Dashboard")
+                        .customFont(.light, 25)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "person")
+                        .onTapGesture {
+                            showProfile.toggle()
+                        }
+                }
+                .padding()
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        Text("Tenant")
+                            .customFont(.medium, 10)
+                        Text(authenticationViewModel.user?.tenantId ?? "Default")
+                            .customFont(.light, 10)
+                    }
+                    
+                    HStack {
+                        Text("Status")
+                            .customFont(.medium, 10)
+                        Text(authenticationViewModel.user?.status ?? "Default")
+                            .customFont(.light, 10)
+                    }
+                }
+                .padding(.horizontal)
+            }
+            .onAppear(perform: {
+                authenticationViewModel.fetchUser()
+            })
+            .navigationDestination(isPresented: $showProfile) {
+                ProfileView().toolbarRole(.editor)
+            }
         }
     }
 }
